b18715fa08c32246214525283b83a8e4719e7caf
artem <SS4T46@yandex.ru>
2026-09-15 12:50:10 +0300

    feat: add convertToPostfix func

--- labac1DM/main.c
+++ labac1DM/main.c
@@ -1,56 +1,160 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <math.h>
-#include "list.h"
+#include "stack.h"
+#include <string.h>
 
-// int main(int argc, char *argv[])
-// {
-//     char *filename = NULL;
-//     char c;
+#define MAX_SIZE 1024
 
-//     ListNode *variables = NULL;
+int Priority(char symbol)
+{
+    switch (symbol)
+    {
+    case '!':
+        return 4;
+    case '&':
+        return 3;
+    case '|':
+        return 2;
+    case '^':
+        return 1;
+    case '(':
+        return 0;
+    default:
+        return -1;
+    }
+}
 
-//     if (strncmp(argv[1], "-file", 5) == 0)
-//     {
-//         filename = strchr(argv[1], '=') + 1;
-//     }
+void convertToPostfix(char infix[], char postfix[])
+{
+    int lenInfix = strlen(infix);
+    StackNodePtr stack = NULL;
 
-//     FILE *fp = fopen(filename, "r");
+    int indexPostfix = 0;
 
-//     if (fp == NULL)
-//     {
-//         printf("Error occured while opening %s", filename);
-//         return 1;
-//     }
-//     else
-//     {
-//         while ((c = getc(fp)) != EOF)
-//         {
-//             if ((strchr("+-^* ", c)) == NULL)
-//             {
-//                 char buf[2] = {c, '\0'};
-//                 variables = appendStr(variables, buf);
-//             }
-//         }
-//     }
+    for (int i = 0; i < lenInfix; i++)
+    {
+        char symbol = infix[i];
+        switch (symbol)
+        {
+        case '!':
+        case '^':
+        case '&':
+        case '|':
+        {
+            while (!isEmpty(stack))
+            {
+                char topValue = stackTop(stack);
 
-//     fclose(fp);
+                if (symbol == '!')
+                {
+                    if (Priority(symbol) < Priority(topValue))
+                    {
+                        postfix[indexPostfix++] = pop(&stack);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    if (Priority(symbol) <= Priority(topValue))
+                    {
+                        postfix[indexPostfix++] = pop(&stack);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            push(&stack, symbol);
+            break;
+        }
+        case ')':
+            while (!isEmpty(stack))
+            {
+                char topValue = stackTop(stack);
+                if (topValue == '(') {
+                    pop(&stack);
+                    break;
+                }
+                postfix[indexPostfix++] = pop(&stack);
+            }
+            break;
+        case '(':
+            push(&stack, symbol);
+            break;
+        default:
+            postfix[indexPostfix++] = symbol;
+        }
+    }
 
-//     printList(variables);
+    while (!isEmpty(stack))
+    {
+        char topValue = stackTop(stack);
+        postfix[indexPostfix++] = pop(&stack);
+    }
+}
 
-//     printf("%d\n", len(variables));
+int main(int argc, char *argv[])
+{
+    char *filename = NULL;
 
+    if (strncmp(argv[1], "-file", 5) == 0)
+    {
+        filename = strchr(argv[1], '=') + 1;
+    }
 
-//     // int countOfOperation = pow(2, len(variables));
-//     int countOfOperation = 1 << len(variables);
-//     // for (int i = 0; i < 1; i++)
-//     // {
-//     //     const char binNumber = convertToBin(i);
-//     //     printf("%08d\n", binNumber[0]);
-//     // }
+    FILE *fp = fopen(filename, "r");
 
-//     printf("\n");
-//     return 0;
-// }
+    if (fp == NULL)
+    {
+        printf("Error occured while opening %s", filename);
+        return 1;
+    }
+
+    char infix[MAX_SIZE];
+    int len = 0;
+    int symbol;
+
+    while ((symbol = getc(fp)) != EOF && len < MAX_SIZE - 1)
+    {
+        if (symbol != ' ')
+        {
+            infix[len++] = (char)symbol;
+            // printf("%c", symbol);
+        }
+    }
+    infix[len] = '\0';
+
+    fclose(fp);
+
+    char postfix[len];
+    // for (int i = 0; i < len; i++)
+    // {
+    //     printf("%c", infix[i]);
+    // }
+
+    convertToPostfix(infix, postfix);
+    for (int i = 0; i < len; i++)
+    {
+        printf("%c ", postfix[i]);
+    }
+
+    // printList(variables);
+
+    // printf("%d\n", len(variables));
+
+    // int countOfOperation = pow(2, len(variables));
+    // int countOfOperation = 1 << len(variables);
+    // for (int i = 0; i < 1; i++)
+    // {
+    //     const char binNumber = convertToBin(i);
+    //     printf("%08d\n", binNumber[0]);
+    // }
+
+    printf("\n");
+    return 0;
+}
