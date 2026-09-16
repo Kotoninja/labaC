2dee29f24fac5e612f1423b1853e32d10f544136
artem <SS4T46@yandex.ru>
2026-09-16 21:26:19 +0300

    feat: add example stage for check

---
+++ labac1DM/bin.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "list.h"
+#include <math.h>
+
+
+int convertToBin(int number)
+{
+    ListNode *digits = NULL;
+
+    while (number != 0)
+    {
+        int remainder = number % 2;
+        digits = appendInt(digits, remainder);
+        number /= 2;
+    }
+
+    int binNumber = 0;
+    int i = 0;
+    int lenDigits = len(digits);
+    while (digits)
+    {
+        binNumber += digits->data.ival * pow(10, lenDigits - 1 - i);
+        i++;
+        digits = digits->next;
+    }
+    return binNumber;
+}
+
+// int main(void)
+// {
+//     {
+//         int number = convertToBin(25);
+//         int expectedNumber = 11001;
+//         printf("%d", number == expectedNumber);
+//     }
+
+//     {
+//         int number = convertToBin(42);
+//         int expectedNumber = 101010;
+//         printf("%d", number == expectedNumber);
+//     }
+
+//     {
+//         int number = convertToBin(5);
+//         int expectedNumber = 101;
+//         printf("%d", number == expectedNumber);
+//     }
+//     printf("\n");
+// }
