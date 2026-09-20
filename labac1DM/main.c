cab22967a7c83d597ba4c921fcdd802a569a5ee8
artem <SS4T46@yandex.ru>
2026-09-20 17:33:30 +0300

    feat: fix and docs main

--- labac1DM/main.c
+++ labac1DM/main.c
@@ -14,7 +14,7 @@
     1. Отдельно считаем количество переменных в выражении (n)
 3. Конвертируем инфиксную запись в постфиксную
 4. Создаем цикл с 2^n итерациями (для построения таблицы истинности). Переводим i-ую итерацию в двоичное число, каждый элемент которого будет в стеке
-5. В i-ой итерации передаем hashmap с переменными и элементами двочиного числа, выражение в функцию convertToPostfix и выводим результат
+5. В i-ой итерации передаем hashmap(с переменными и элементами двочиного числа) и выражение в функцию convertToPostfix и выводим результат
 
 ! - инверсия
 | - дизъюнкция
@@ -36,26 +36,28 @@ int Priority(char symbol)
 {
     switch (symbol)
     {
-    case '!':
+    case '!': // инверсия
         return 7;
-    case '&':
+    case '&': // конъюнкция
         return 6;
-    case '|':
-    case '^':
+    case '|': // дизъюнкция
+    case '^': // сложение по модулю 2
         return 5;
-    case '<':
-    case '>':
+    case '<': // штрих Шеффера
+    case '>': // стрелка Пирса
         return 4;
-    case '*':
-    case '~':
+    case '*': // импликация
+    case '~': // коимпликация
         return 2;
-    case '@':
+    case '@': // эквивалентность
         return 1;
     default:
         return 0;
     }
 }
-
+/// @brief Возвращает является ли c оператором
+/// @param c
+/// @return
 int isOperator(char c)
 {
     if (c >= 'a' && c <= 'z')
@@ -65,23 +67,41 @@ int isOperator(char c)
     return 1;
 }
 
+/// @brief Считает логическое выражение между двумя(или одним) операндоми
+/// @param op1
+/// @param op2
+/// @param operator
+/// @return
 int calculate(int op1, int op2, char operator)
 {
     switch (operator)
     {
-    case '!':
+    case '!': // инверсия
         return !op1;
-    case '&':
+    case '&': // конъюнкция
         return op1 && op2;
-    case '|':
+    case '|': // дизъюнкция
         return op1 || op2;
-    case '^':
+    case '^': // сложение по модулю 2
         return (!op1 && op2) || (op1 && !op2);
+    case '@': // эквивалентность
+        return (op1 && op2) || (!op1 && !op2);
+    case '*': // импликация
+        return (!op1 || op2);
+    case '~': // коимпликация
+        return (op1 && !op2);
+    case '>': // стрелка Пирса
+        return !(op1 || op2);
+    case '<': // штрих Шеффера
+        return !(op1 && op2);
     default:
         return -1;
     }
 }
-
+/// @brief Высчитывает логическое выражение
+/// @param postfix
+/// @param hashMap
+/// @return
 int calculateExperssion(char postfix[], hash_map_t *hashMap)
 {
     assert(hashMap != NULL);
@@ -144,6 +164,7 @@ void convertToPostfix(char infix[], char postfix[])
                 if (topValue == '(')
                 {
                     pop(&stack);
+                    break;
                 }
                 postfix[indexPostfix++] = pop(&stack);
             }
@@ -194,6 +215,7 @@ void convertToPostfix(char infix[], char postfix[])
         char topValue = stackTop(stack);
         postfix[indexPostfix++] = pop(&stack);
     }
+    postfix[indexPostfix] = '\0';
 }
 
 /// @brief Сборка всей логики
@@ -220,12 +242,14 @@ int main(int argc, char *argv[])
     }
 
     // Создаем массив куда записываются данные из файла (в массиве только операции и операнды)
-    char infix[MAX_SIZE];
+    char infix[MAX_SIZE]; // Строка для инфиксного(исходного) выражения
     int len = 0;
-    int symbol;
-    char variables[MAX_SIZE];
+    int symbol; // Вспомогательная переменная для чтения файла
+
+    char variables[MAX_SIZE]; // Строка, где хранятся все переменные
     int countOfVariables;
-    hash_map_t *frequency = hash_map_create(1);
+
+    hash_map_t *frequency = hash_map_create(1); // Hashmap для подсчета частоты символов (чтобы не было повторок переменных)
 
     while ((symbol = getc(fp)) != EOF && len < MAX_SIZE - 1)
     {
@@ -233,28 +257,17 @@ int main(int argc, char *argv[])
         {
             infix[len++] = (char)symbol;
             // Cчитаем количество переменных
-            switch (symbol)
+            if (!isOperator(symbol))
             {
-            case '!':
-            case '&':
-            case '(':
-            case ')':
-            case '^':
-            case '|':
-            {
-                continue;
-            }
-            default:
-            {
-                if (!hash_map_key(frequency, (char[]){symbol, '\0'}))
+                if (!hash_map_key(frequency, (char[]){symbol, '\0'})) // Если есть повторка - скипаем
                 {
                     variables[countOfVariables++] = symbol;
                     frequency = hash_map_insert(frequency, (char[]){symbol, '\0'}, 0);
                 }
             }
-            }
         }
     }
+
     infix[len] = '\0';
     variables[countOfVariables] = '\0';
     hash_map_free(frequency);
@@ -264,8 +277,9 @@ int main(int argc, char *argv[])
     char postfix[len];
 
     convertToPostfix(infix, postfix);
-    int countOfOperation = 1 << countOfVariables;
+    int countOfOperation = 1 << countOfVariables; // Считаем количество операци в таблице истинности
 
+    // Формируем таблицу
     for (int i = 0; i < countOfVariables; i++)
     {
         printf("%c ", variables[i]);
@@ -274,13 +288,15 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < countOfOperation; i++)
     {
-        StackNodePtr stackBinNumber = convertToBin(i, countOfVariables);
+        StackNodePtr stackBinNumber = convertToBin(i, countOfVariables); // Создаем ряд чисел для переменных (типо 1 0 0 0 или 0 0 1 1)
+        // Присвиваем каждой переменной значение
         hash_map_t *hashmapVariables = hash_map_create(countOfVariables);
         printStack(stackBinNumber);
         for (int i = 0; i < countOfVariables; i++)
         {
             hashmapVariables = hash_map_insert(hashmapVariables, (char[]){variables[i], '\0'}, pop(&stackBinNumber));
         }
+        // Результат выражения
         printf("   %d\n", calculateExperssion(postfix, hashmapVariables));
     }
 
